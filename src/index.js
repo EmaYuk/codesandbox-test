79fafa88265076e1dd534f5be858c0d9b19e15ed
EmaYuk <89290400+EmaYuk@users.noreply.github.com>
2021-12-23 00:27:48 +0900

    関数デフォルト設定部分まで

--- src/index.js
+++ src/index.js
@@ -77,10 +77,47 @@
 // console.log(func1("func1です"));
 
 //アロー関数
-const func2 = (str) => {
-  return str;
-};
-console.log(func2("func2です"));
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
 
-const func3 = (num1, num2) => num1 + num2;
-console.log(func3(5, 6));
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(5, 6));
+
+/**
+ * 分割代入
+ */
+
+// オブジェクトversion
+// const myProfile = {
+//   name: "Ema",
+//   age: 22
+// };
+
+// // 長い
+// // const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// // console.log(message1);
+
+// // 分割代入
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+//配列version
+// const myProfile = ["Ema", 22];
+
+// // 長い
+// // const message1 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// // console.log(message1);
+
+// // 分割代入
+// const [name, age] = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん！`);
+// sayHello();
